4a16a90bd9af7dfe6043b4cf30ca8e9e67620416
Ingrid Elise Vennonen <ingridvennonen@gmail.com>
2023-05-02 16:42:08 +1000

    First commit for new KEEL repo

---
+++ functions/getTemperature.ts
@@ -0,0 +1,23 @@
+import { GetTemperature } from "@teamkeel/sdk";
+
+export default GetTemperature(async (input, api, ctx) => {
+	 // get the lat and long from the input type
+	 const { latitude, longitude } = input;
+	const params = "airTemperature";
+
+	// call API
+	const res = await api.fetch(
+		`https://api.stormglass.io/v2/weather/point?lat=${latitude}&lng=${longitude}&params=${params}`,
+		{ 
+			headers: {
+			Authorization:
+			  "411512a6-b996-11ea-9409-0242ac130002-41151364-b996-11ea-9409-0242ac130002",
+		  },
+		}
+	);
+
+	//return whatever API returns
+	return res.json()
+	  
+});
+	
--- functions/setCompletion.ts
+++ functions/setCompletion.ts
@@ -1,25 +1,25 @@
-import { SetCompletion, Todo } from "@teamkeel/sdk";
+// import { SetCompletion, Todo } from "@teamkeel/sdk";
 
-export default SetCompletion(async (inputs, api, ctx) => {
-  const now = new Date();
+// export default SetCompletion(async (inputs, api, ctx) => {
+//   const now = new Date();
   
-  const todo = await api.models.todo.findOne(inputs.where);
+//   const todo = await api.models.todo.findOne(inputs.where);
 
-  if (todo && todo.ownerId == ctx.identity?.id) {
-    api.permissions.allow();
-  } else {
-    api.permissions.deny();
-  }
+//   if (todo && todo.ownerId == ctx.identity?.id) {
+//     api.permissions.allow();
+//   } else {
+//     api.permissions.deny();
+//   }
 
-  const values: Partial<Todo> = {
-    ...inputs.values,
-  };
+//   const values: Partial<Todo> = {
+//     ...inputs.values,
+//   };
 
-  if (inputs.values.complete) {
-    values.completedAt = now;
-  }
+//   if (inputs.values.complete) {
+//     values.completedAt = now;
+//   }
 
-  return api.models.todo.update(inputs.where, {
-    ...values,
-  });
-});
+//   return api.models.todo.update(inputs.where, {
+//     ...values,
+//   });
+// });
